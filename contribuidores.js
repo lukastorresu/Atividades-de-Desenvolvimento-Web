27dfa8d7af2cafef13e9dec69b4f60a7b748265f
Luiz Henrique Serafim <luizhserafim2001@hotmail.com>
2022-10-28 21:51:47 -0300

    Adicionei meu Site (Site de Musica) ao repositorio e alterei os contribuidores

--- contribuidores.js
+++ contribuidores.js
@@ -1,4 +1,5 @@
 contribuidores = [
     { username: 'lukastorresu', message: 'Happy Hacktoberfest yall :)' },
-    { username: 'mateus-sm', message: 'Happy Hacktoberfest 2022!' }
+    { username: 'mateus-sm', message: 'Happy Hacktoberfest 2022!' },
+    { username: 'LHSerafim21', message: 'Happy Hacktoberfest 2022!' }
 ];
